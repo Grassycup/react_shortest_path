2ea724244eb137bdfabf9b2224c879db0ac551ba
Sung <grassycup@gmail.com>
2017-07-06 00:30:53 -0400

    Experment 1
    Experimenting with material-ui and testing compatibility with cytoscape

--- src/App.js
+++ src/App.js
@@ -2,21 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Heapd3 from './visual/heapd3'
 import './App.css';
-import Graph from './data_structure/graph.js';
-import Heap from './data_structure/heap.js'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './page/main/main.js';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import HeapTabComponent from './page/heap/heaptab.js';
+import AppBar from 'material-ui/AppBar';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/*<div className="App-header">*/}
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          {/*<h2>Welcome to React</h2>*/}
-        {/*</div>*/}
-        {/*<p className="App-intro">*/}
-          {/*To get started, edit <code>src/App.js</code> and save to reload.*/}
-        {/*</p>*/}
         <Heapd3 />
+        <MuiThemeProvider>
+          <AppBar
+            title="Happy App"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+          />
+        </MuiThemeProvider>
+        <MuiThemeProvider>
+          <HeapTabComponent />
+        </MuiThemeProvider>
+
+
       </div>
     );
   }
